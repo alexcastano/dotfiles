84714f17dc89e33b9f76f6bca71d9aad349146ac
Alejandro Castaño del Castillo <alex@batkiwi.com>
2014-01-06 01:53:17 +0100

    port_redirect

--- bin/redirect_3000.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo iptables -t nat -F
-sudo iptables -t nat -I OUTPUT -p tcp -o lo --dport 80 -j REDIRECT --to-ports 3000
-sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 3000
